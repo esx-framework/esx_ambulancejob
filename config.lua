aa12420a01a170a0661f334664b31e754c4e9944
Mycroft <kaseyfitton@hotmail.co.uk>
2023-03-02 13:16:45 -0800

    fix:(client/config): change Local vehicle spawn to be vector4
    Adds Heading option

--- config.lua
+++ config.lua
@@ -68,7 +68,7 @@ Config.Hospitals = {
 		Vehicles = {
 			{
 				Spawner = vector3(307.7, -1433.4, 30.0),
-				InsideShop = vector3(446.7, -1355.6, 43.5),
+				InsideShop = vector4(452.3055, -1360.1731, 43.5538, 319.1165),
 				Marker = {type = 36, x = 1.0, y = 1.0, z = 1.0, r = 100, g = 50, b = 200, a = 100, rotate = true},
 				SpawnPoints = {
 					{coords = vector3(297.2, -1429.5, 29.8), heading = 227.6, radius = 4.0},
@@ -81,7 +81,7 @@ Config.Hospitals = {
 		Helicopters = {
 			{
 				Spawner = vector3(317.5, -1449.5, 46.5),
-				InsideShop = vector3(305.6, -1419.7, 41.5),
+				InsideShop = vector4(343.7769, -1423.0913, 76.1742, 322.8178),
 				Marker = {type = 34, x = 1.5, y = 1.5, z = 1.5, r = 100, g = 150, b = 150, a = 100, rotate = true},
 				SpawnPoints = {
 					{coords = vector3(313.5, -1465.1, 46.5), heading = 142.7, radius = 10.0},
