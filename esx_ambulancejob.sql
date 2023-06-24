7b8ef0164d6e62bb77685a7bba4ae8cc96d4ec1d
mx developement <62698394+TrymTube@users.noreply.github.com>
2023-06-24 10:00:36 +0200

    fixed something

--- esx_ambulancejob.sql
+++ esx_ambulancejob.sql
@@ -10,11 +10,11 @@ INSERT INTO `datastore` (name, label, shared) VALUES
 	('society_ambulance', 'Ambulance', 1)
 ;
 
-INSERT INTO `job_grades` (job_name, grade, name, label, salary) VALUES
-	('ambulance',0,'ambulance','Ambulancier',20),
-	('ambulance',1,'doctor','Medecin',40),
-	('ambulance',2,'chief_doctor','Medecin-chef',60),
-	('ambulance',3,'boss','Chirurgien',80)
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('ambulance',0,'ambulance','Ambulancier',20,'{}','{}'),
+	('ambulance',1,'doctor','Medecin',40,'{}','{}'),
+	('ambulance',2,'chief_doctor','Medecin-chef',60,'{}','{}'),
+	('ambulance',3,'boss','Chirurgien',80,'{}','{}')
 ;
 
 INSERT INTO `jobs` (name, label) VALUES
