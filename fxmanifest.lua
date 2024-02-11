b330b3fc9abba741be84c9d0a0290d6a5329a709
Kasey Fitton <kaseyfitton@hotmail.co.uk>
2024-02-11 16:05:41 +0000

    tweak(fxmanifest): update description

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version 'adamant'
 game 'gta5'
-description 'ESX Ambulance Job'
+description 'Provides a way for players to RP as paramedics (heal/revive players)'
 version '1.0.1'
 legacyversion '1.9.1'
 lua54 'yes'
